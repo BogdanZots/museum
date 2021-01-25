79d6d121e32afc51327beaf7fc1dec8205989d61
BogdanZots <54004090+BogdanZots@users.noreply.github.com>
2021-01-25 13:00:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+	var swiperUpcoming_1 = new Swiper('.swiper-upcoming_1', {
+    slidesPerView: 4,
+    spaceBetween: 36,
+    loop: true,
+      navigation: {
+        nextEl: '.swiper-upcoming_1__arrows',
+
+      },
+    });
+    var swiperUpcoming_1 = new Swiper('.intro-slider', {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 36,
+        navigation: {
+          nextEl: '.slider-arrows__right',
+          prevEl: '.slider-arrows__left',
+  
+        },
+      });
